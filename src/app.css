f94e1ec90ac7c77c265a9bba2a6529b5a7d1c301
Souvik Giri <74680826+SouvikGiri@users.noreply.github.com>
2021-08-31 16:52:27 +0530

    Add files via upload

---
+++ src/app.css
@@ -0,0 +1,83 @@
+@import url('https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital@1&family=Rampart+One&family=Slabo+27px&display=swap');
+* {
+    margin: 0%;
+    padding: 0%;
+}
+body{
+background-image: url("i28.jpg");
+background-size: cover ; 
+background-repeat: no-repeat;
+
+}
+  .main {
+    font-family: 'Ubuntu', sans-serif;
+    color: white;
+    height: 70px;
+    padding: 20px;
+    padding-top: 20px;
+  }
+  h1 {
+    float: left;
+    padding: 10px;
+    color: white;
+    cursor:pointer;
+    border-radius: 15px;
+  }
+  .btn {
+    float: right;
+    padding: 10px;
+    background-color:purple;
+    cursor: pointer;
+    border-radius: 5px;
+  }
+  .btn:hover {
+    background-color:black;
+  }
+  .row {
+    margin-top: -20px;
+    margin-left: 100px;
+  }
+  .column {
+    float: left;
+    width: 25%;
+    padding: 10 10px;
+    margin: 20px;
+  }
+  .card {
+    box-shadow: 0 15px 35px rgba(0,0,0,0.2);
+    padding: 30px;
+    text-align: center;
+    background-color: #D5ECF5;
+    border-radius: 15px 50px;
+  }
+
+  @media screen and (max-width: 600px) {
+    .column {
+      width: 100%;
+      display: block;
+      margin-bottom: 20px;
+    }
+  }
+  .card-img-top {
+    border-radius: 50%;
+    height: 50%;
+    width: 50%;
+    border: 4px solid black;
+  }
+  .card-title {
+    font-family: LibreBaskerville;
+    font-size: 23px;
+  }
+  .card-text {
+    color: grey;
+    font-family: 'Ubuntu', sans-serif;
+    font-size: 19px;
+  }
+  .card:hover{
+    box-shadow: 4px 3px black;
+    cursor: pointer;
+  }
+  .card:hover .card-text{
+    color: purple;
+
+  }
